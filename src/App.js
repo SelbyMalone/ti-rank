099bb697192739b79602866ec8487efce0d7714f
SirrMelon <selbster99@gmail.com>
2024-06-21 01:42:57 +1000

    started actual front end work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Game History
+            </header>
+            <div className="App-body">
+                <div className="App-main-content">
+                </div>
+                <div className="App-sidebar">
+                    <p>Jade	1,121</p>
+                    <p>Mike	1,096</p>
+                    <p>Fry	1,065</p>
+                    <p>Jayden	1,052</p>
+                    <p>Keagan	1,035</p>
+                    <p>Will D	1,035</p>
+                    <p>Selby	1,030</p>
+                    <p>Nick	1,016</p>
+                    <p>Tiana	1,010</p>
+                    <p>Aaron	1,008</p>
+                    <p>Jane	978</p>
+                    <p>John	973</p>
+                    <p>Jordan	967</p>
+                    <p>Will R	962</p>
+                    <p>Peter	960</p>
+                    <p>Bob	938</p>
+                    <p>Doug	934</p>
+                    <p>Kai	928</p>
+                    <p>Rob	887</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
