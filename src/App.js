3eb5a56cfab92e9eabbf6929dbeb31dc7d443bf8
SirrMelon <selbster99@gmail.com>
2024-06-26 19:19:16 +1000

    player ranks now loads automatically
    currently only shows ranked players, need to add unranked players and pretty it up a bit with conditional formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
-import { queryFirestore } from './firebaseScripts'
+import { usePlayers } from './firebaseScripts'
 
 
 function App() {
+    const { rankedPlayers } = usePlayers();
+
+    // format numbers with commas
+    const formatNumberWithCommas = (number) => {
+        return new Intl.NumberFormat('en-US').format(number);
+    };
+
     return (
         <div className="app row-flex">
             <div className="main-content">
@@ -17,27 +24,13 @@ function App() {
                 <div className="header">
                     Ranks
                 </div>
-                <button onClick={queryFirestore}>Query Firestore</button>
 
-                <p>Jade	1,121</p>
-                <p>Mike	1,096</p>
-                <p>Fry	1,065</p>
-                <p>Jayden	1,052</p>
-                <p>Keagan	1,035</p>
-                <p>Will D	1,035</p>
-                <p>Selby	1,030</p>
-                <p>Nick	1,016</p>
-                <p>Tiana	1,010</p>
-                <p>Aaron	1,008</p>
-                <p>Jane	978</p>
-                <p>John	973</p>
-                <p>Jordan	967</p>
-                <p>Will R	962</p>
-                <p>Peter	960</p>
-                <p>Bob	938</p>
-                <p>Doug	934</p>
-                <p>Kai	928</p>
-                <p>Rob	887</p>
+                {rankedPlayers.map(player => (
+                    <div key={player.name} className="player-box">
+                        <span className="player-name">{player.name}</span>
+                        <span className="player-rank">{formatNumberWithCommas(player.rank)}</span>
+                    </div>
+                ))}
             </div>
         </div>
     );
