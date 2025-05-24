19b4f5ef7c77b09b77aa1af89b3521edd256a476
SelbyMalone <31752555+SelbyMalone@users.noreply.github.com>
2025-05-24 18:58:54 +1000

    removed firebase_db.js

--- src/js/firebase_db.js
+++
@@ -1,6 +0,0 @@
-import { app, analytics } from './firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
-import { getFirestore } from "firebase/firestore";
-
-// Initialize Firestore
-const db = getFirestore(app);
