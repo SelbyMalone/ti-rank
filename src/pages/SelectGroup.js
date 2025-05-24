9a35f88fe5588a566fc8c25203ede41a60f9b202
SelbyMalone <31752555+SelbyMalone@users.noreply.github.com>
2025-05-24 16:14:56 +1000

    Edit SelectGroup.js

--- src/pages/SelectGroup.js
+++ src/pages/SelectGroup.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 //redirect to group, name pending
-
-
-function SelectGroup () {
-    const navigate = useNavigate
+function SelectGroup() {
+    const navigate = useNavigate();
 
     const handleNavigate = (groupID) => {
-      navigate('/${groupID}');
-    }
+        navigate(`/${groupID}`);
+    };
+
     return (
         <div>
-            <button onClick={() handleNavigate('uYmHldpJVY2K0uiEH99L')}>
+            <button onClick={() => handleNavigate('uYmHldpJVY2K0uiEH99L')}>
                 mainGroup
             </button>
-            <button onClick{() handleNavigate('rCICxM6t8QsyGfwiL8qa')}
+            <button onClick={() => handleNavigate('rCICxM6t8QsyGfwiL8qa')}>
+                testGroup
+            </button>
         </div>
-    )
+    );
 }
-export default SelectGroup
+
+export default SelectGroup;
